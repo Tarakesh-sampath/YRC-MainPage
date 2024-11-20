39f806dc946aeaa4003d64feeb1fd60d0f7ae805
Tarakeshwaran <tarakeshsampath1@gmail.com>
2024-11-20 21:14:46 +0530

    added logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-
 import './App.css';
-
 function App() {
   return (
     <div className="App">
+      <img src={require("./images/yrc_logo.svg").default} className="App-logo" alt="logo" />
     </div>
   );
 }
