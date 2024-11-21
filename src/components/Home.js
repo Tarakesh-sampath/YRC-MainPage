3711939dd6fe57b5ccfd773b3a95fb4120a729f3
Tarakeshwaran <tarakeshsampath1@gmail.com>
2024-11-21 07:38:12 +0530

    added for home page component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import Navbar from './Navbar.js';
+
+function Home() {
+  return (
+    <div>
+        <Navbar/>
+      Hello world
+    </div>
+  )
+}
+
+export default Home
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,8 @@ function Navbar() {
       <div className='navbar'>
         <img src={require("../images/vec_logo_bg.svg").default} style={{ borderRadius: '50%', padding: "5px 5px"}} className="vec-logo" alt="logo" height={65} />
         <nav>
-          <ul>
-            <li><Link to="/">AboutUs</Link></li>
-          </ul>
+            <Link to="/">AboutUs</Link>
+            <Link to="/Home">Home</Link>
         </nav>
         <img src={require("../images/yrc_logo.svg").default} className="yrc-logo" alt="logo" height={80} />
       </div>
