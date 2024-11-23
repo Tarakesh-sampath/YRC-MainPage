e0ed2a3abcc5c1d6a5f991d34176aa472b841f73
Tarakeshwaran <tarakeshsampath1@gmail.com>
2024-11-23 15:02:48 +0530

    most completed top nav bar

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React ,{ useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import "./Dropdown.css"
 
-function Dropdown() {
+function Dropdown( props ) {
+  const[click,setClick] = useState(false)
+  const handleclick = () => setClick(!click)
   return (
-    <div>
-      
+    <div className='dropdown-box'>
+      <ul onClick={handleclick} classname={click ? "dropdown-a" : "dropdown"}>
+        {props.list.map((item,index) => {
+          return(
+              <li key={index}>
+                <Link className={item.cName} to={item.path} onClick={() => setClick(false)}>{item.title}</Link>
+              </li>
+          );
+        })}
+      </ul>
     </div>
   )
 }
-
+// Correct definition of propTypes
+Dropdown.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      path: PropTypes.string.isRequired,
+      cName: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 export default Dropdown
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,18 @@ import Dropdown from "./Dropdown.js";
 import "./Navbar.css";
 
 function Navbar() {
+  const OurService = [
+        {
+            title:"action - 1",
+            path:"/",
+            cName:"Dropdown-link"
+        },
+        {
+            title:"action - 2",
+            path:"/Home",
+            cName:"Dropdown-link"
+        }
+    ]
     const[click,setClick] = useState(false)
     const[dropdown,setDropdown] = useState(false)
     const handleclick = () => setClick(!click)
@@ -27,28 +39,49 @@ function Navbar() {
         <>
         <header>
             <div className='navbar'>
-            <img src={require("../images/vec_logo_bg.svg").default} style={{ borderRadius: '50%', padding: "5px 5px"}} className="vec-logo" alt="logo" height={65} />
+            <img 
+              src={require("../images/vec_logo_bg.svg").default}
+              style={{ borderRadius: '50%', padding: "5px 5px"}} 
+              className="vec-logo" 
+              alt="logo" 
+              height={65} 
+            />
             <div className='navbar-body'>
                 <div className='menu-icon' onClick={handleclick}>
                     <ion-icon Name={click ? "close-outline": "menu-sharp"}/>
                 </div>
                 <ul className={click ? "nav-menu-a" : "nav-menu"}>
                     <li className='nav-item'>
-                    <Link to="/Home" className='nav-links' onClick={closeMobileMenu}>Home</Link>
+                      <Link to="/Home" className='nav-links' onClick={closeMobileMenu}>
+                        Home
+                      </Link>
                     </li>
                     <li className='nav-item'>
-                    <Link to="/" className='nav-links' onClick={closeMobileMenu}>AboutUs</Link>
+                      <Link to="/" className='nav-links' onClick={closeMobileMenu}>
+                        AboutUs
+                      </Link>
                     </li>
-                    <li className='nav-item' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-                    <Link to="/" className='nav-links' onClick={closeMobileMenu}>Our Services</Link>
-                    {dropdown && <Dropdown/>}
+                    <li 
+                      className='nav-item' 
+                      onMouseEnter={onMouseEnter}
+                      onMouseLeave={onMouseLeave}
+                    >
+                      <Link to="/" className='nav-links' onClick={closeMobileMenu}>
+                        Our Services
+                      </Link>
+                      {dropdown && <Dropdown list={OurService}/>}
                     </li>
                     <li className='nav-item'>
                     <Link to="/" className='nav-links' onClick={closeMobileMenu}>Contact us</Link>
                     </li>
                 </ul>
             </div>
-            <img src={require("../images/yrc_logo.svg").default} className="yrc-logo" alt="logo" height={80} />
+            <img 
+              src={require("../images/yrc_logo.svg").default}
+              className="yrc-logo" 
+              alt="logo" 
+              height={80} 
+            />
             </div>
         </header>
         </>
